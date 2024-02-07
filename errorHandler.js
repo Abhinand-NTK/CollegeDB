97237bd240a5ec6ecb3d9a92a6397d35e5ed8751
AbhinandNtk <abhinandntkm@gmail.com>
2024-02-07 09:59:56 +0530

    Git init file folder changed

---
+++ errorHandler.js
@@ -0,0 +1,12 @@
+// errorHandler.js
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+  
+    res.status(500).json({
+      error: 'Internal Server Error',
+    });
+  };
+  
+module.exports = errorHandler;
+  
